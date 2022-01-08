ff95367967b75918bc3feab613a135ee523decb0
adarshares <adarshares@gmail.com>
2022-01-09 03:51:46 +0530

    add event frontend done

---
+++ src/app/main/events/store/reducers/event.reducer.js
@@ -0,0 +1,32 @@
+import * as Actions from '../actions';
+
+const initialState = {
+    data: null
+};
+
+const eventReducer = function (state = initialState, action) {
+    switch ( action.type )
+    {
+
+        case Actions.SAVE_Event:
+        {
+            return {
+                ...state,
+                data: action.payload
+            };
+        }
+        case Actions.GET_Event:
+        {
+            return {
+                ...state,
+                data: action.payload
+            };
+        }
+        default:
+        {
+            return state;
+        }
+    }
+};
+
+export default eventReducer;
