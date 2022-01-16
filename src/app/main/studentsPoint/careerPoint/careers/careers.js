3ba1b0ea658cc88c5e91f025f70e80e405fc5df4
Aryan Singh <ary1733@gmail.com>
2022-01-16 14:23:13 +0530

    minor changes

--- src/app/main/studentsPoint/careerPoint/careers/careers.js
+++ src/app/main/studentsPoint/careerPoint/careers/careers.js
@@ -218,7 +218,6 @@ function Careers(props)
                                                                 method: 'GET',
                                                                 responseType: 'blob', // important
                                                             }).then((response) => {
-                                                                console.log(response)
                                                                 const url = window.URL.createObjectURL(new Blob([response.data]));
                                                                 const link = document.createElement('a');
                                                                 link.href = url;
